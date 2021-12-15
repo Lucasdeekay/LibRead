d5d97d7e8a0839fb535ca211408cfb5339291d6c
Kolawole Dennis Olukayode <lucasdeekay98@gmail.com>
2021-12-15 09:59:32 +0100

    Edited urls.py

--- Library/urls.py
+++ Library/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('login/forgot-password', views.forgotten_password, name='forgot_password'),
     path('login/forgot-password/retrieve-password', views.password_retrieval, name='password_retrieval'),
+    path('login/forgot-password/retrieve-password/update', views.update_password, name='update_password'),
     path('register', views.register, name='register'),
     path('repository', views.repository, name='repository'),
     path('repository/e-books', views.offline_resources, name='offline_resources'),
