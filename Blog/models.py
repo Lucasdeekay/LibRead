15bbe4d5e933d5f7d3eef0efdcedba453e5db99a
Kolawole Dennis Olukayode <lucasdeekay98@gmail.com>
2021-12-23 00:57:30 +0100

    Commiting file

--- Blog/models.py
+++ Blog/models.py
@@ -5,7 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=250)
     article = models.CharField(max_length=1000)
     date = models.DateTimeField()
-    image = models.ImageField(upload_to='LibRead/images/', null=True, blank=True)
+    image = models.ImageField(upload_to='LibRead/images/')
 
     def __str__(self):
         return f'{self.title}'
